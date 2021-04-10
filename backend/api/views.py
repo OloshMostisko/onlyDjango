f900cc57fed4aacbb048b1359dd41afe84c70aa9
Shezan Mahmud <shezan@constantmd.com>
2021-04-10 13:46:40 +0600

    Project and app created

---
+++ backend/api/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.views import generic
+from .models import *
+from django.urls import reverse
+
+# Create your views here.
+
+
+def index(request):
+   
+   # posts = ChairmanMsg.objects.all()
+    
+    context = {
+     #  'posts': posts
+    }
+  
+    return render(request, 'index.html',context)
